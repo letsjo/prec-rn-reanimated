d89dbfea2aa742fb52d62e5ada7ea329b0076b23
letsjo <hyunoh.jo@gmail.com>
2023-02-21 00:24:07 +0900

    feat(*): ScrollView practice

---
+++ 03 scrollView/TestScrollView.js
@@ -0,0 +1,37 @@
+import { StyleSheet } from 'react-native'
+import Animated, { useAnimatedScrollHandler, useSharedValue } from 'react-native-reanimated';
+import Page from './scrollView/Page';
+
+const WORDS = ["What's", 'up', 'mobile', 'devs?']
+
+const TestScrollView = () => {
+
+  const translateX = useSharedValue(0);
+
+  const scrollHandler = useAnimatedScrollHandler((event) => {
+    translateX.value = event.contentOffset.x;
+  })
+
+  return (
+    <Animated.ScrollView
+      pagingEnabled
+      horizontal
+      onScroll={ scrollHandler }
+      scrollEventThrottle={ 16 }
+      style={ styles.container }
+    >
+      { WORDS.map((title, index) => {
+        return <Page key={ index.toString() } title={ title } index={ index } translateX={ translateX } />
+      }) }
+    </Animated.ScrollView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
+
+export default TestScrollView
